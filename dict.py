29ef20ab5f20e900026d1f0af6ea64e4e1fbf5b1
stephanielewisthelma <slewisidc230004@gmail.com>
2025-08-13 13:48:31 +0100

    dictionary

--- dict.py
+++ dict.py
@@ -3,6 +3,20 @@ car = {
     "model":"corolla",
     "year":"2020",
     "automatic-trans":True,
-    "year":2030
+    "year":2030,
+    "colors":["red","blue","white"]
 }
 # print (car)
+# print(type(car))
+# print(len(car))
+# print(car.get("model"))
+
+# get all keys
+# ky = car.keys();
+# print(ky)
+
+# va = car.values();
+# print(va)
+
+car ["tinted"]=["factory","grade 0", "grade 1"]
+print(car)
