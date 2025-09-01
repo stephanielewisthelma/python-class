ae9895f1b8659808665d3d2cc1e0934587a72509
stephanielewisthelma <slewisidc230004@gmail.com>
2025-09-01 13:19:16 +0100

    numpy clsw

--- nump.py
+++ nump.py
@@ -9,10 +9,10 @@ import numpy as np
 # print (arr)
 
 arr = np.array ([[1,2,3] ,[4,5,6]])
-# print (arr)
+print (arr)
 
 arr = np.array ([[[1,2,3] ,[4,5,6], [7,8,9]]])
-# print (arr.ndim)
+print (arr.ndim)
 
 
 # arr = np.array([1,2,3],ndim=6)
